6daa0ebe4a1325682f58e750a9c74e8b097c3468
Thomas <thomasr928@gmail.com>
2023-12-19 13:47:45 -0600

    Fix script for generating full merkle maps

--- contracts/src/genmap.ts
+++ contracts/src/genmap.ts
@@ -1,16 +1,16 @@
 import fs from 'fs'
+import * as assert from 'assert';
 
 import {
     shutdown,
     Field,
-    MerkleMap,
 } from 'o1js';
 
-function buildMerkleMap(fnLoad: string): MerkleMap {
-    const map = new MerkleMap();
+import { MerkleMapSerializable, serialize, deserialize } from './merkle_map_serializable.js';
+
+function buildMerkleMap(fnLoad: string): MerkleMapSerializable {
+    const map = new MerkleMapSerializable();
     const dataArray = JSON.parse(fs.readFileSync(fnLoad, 'utf-8'))
-    //const dataArray = JSON.parse(fs.readFileSync('./lookup_table_basic.json', 'utf-8'))
-    console.log("DATA ARRAY", dataArray.length);
     let counter = 0;
     for (var key in dataArray) {
         if (dataArray.hasOwnProperty(key)) {
@@ -19,7 +19,7 @@ function buildMerkleMap(fnLoad: string): MerkleMap {
             map.set(Field(key), Field(dataArray[key]));
             counter += 1
             // Break early for testing...
-            // if (counter > 30) {
+            // if (counter > 5) {
             //     break
             // }
         }
@@ -27,40 +27,39 @@ function buildMerkleMap(fnLoad: string): MerkleMap {
     return map
 }
 
-// TODO - we need serialization for merkle maps so we can save+load them
-function serializeMerkleMap(map: MerkleMap, fnWrite: string) {
-    const jsonString = JSON.stringify(map);
-    fs.writeFileSync(fnWrite, jsonString);
-}
 
-function deserializeMerkleMap(fnRead: string): MerkleMap {
-    const jsonData = fs.readFileSync(fnRead, 'utf8');
-    const map = JSON.parse(jsonData) as MerkleMap;
-    // By default map will have strings instead of Field objects
-    return map;
-}
-
-
+// Load fns - raw dict containing {key: value} lookups
 const fnBasic = 'lookup_table_basic.json'
 const fnFlush = 'lookup_table_flushes.json'
-const merkleMapBasic = buildMerkleMap(fnBasic);
-const merkleMapFlush = buildMerkleMap(fnFlush);
-
-
-//const test_hand = 41 * 41 * 41 * 41 * 2 * 3 * 5;
-// Same example we used in python script - lookup value should be 10
-const four_aces = 41 * 41 * 41 * 41 * 37
-const ret = merkleMapBasic.get(Field(four_aces));
-console.log("TEST HAND", ret);
-
-
+// Write fns - serialized merkle maps
 const merkleMapBasicFn = "merkleMapBasic.json"
 const merkleMapFlushFn = "merkleMapFlush.json"
 
-serializeMerkleMap(merkleMapBasic, merkleMapBasicFn)
-serializeMerkleMap(merkleMapFlush, merkleMapFlushFn)
+// Build and serialize+write merkle maps
+const merkleMapBasic = buildMerkleMap(fnBasic);
+const jsonStringBasic = serialize(merkleMapBasic);
+fs.writeFileSync(merkleMapBasicFn, jsonStringBasic);
 
-const merkleMapBasic_ = deserializeMerkleMap(merkleMapBasicFn);
-const merkleMapFlush_ = deserializeMerkleMap(merkleMapFlushFn);
+const merkleMapFlush = buildMerkleMap(fnFlush);
+const jsonStringFlush = serialize(merkleMapFlush);
+fs.writeFileSync(merkleMapFlushFn, jsonStringFlush);
+
+
+// Sanity checks - make sure we have a valid lookup value
+// and make sure we can deserialize and lookup again
+
+// Same example we used in python script - lookup value should be 10
+//const four_aces = 48;
+const fourAces = 41 * 41 * 41 * 41 * 37
+const lookupVal = merkleMapBasic.get(Field(fourAces));
+console.log(lookupVal.toString());
+assert.equal(lookupVal.toString(), "10", 'Initial lookup should be 10');
+
+// Only going to check the basic lookups - should be fine for flush too if this works
+const jsonDataBasic = fs.readFileSync(merkleMapBasicFn, 'utf8');
+const merkleMapBasic_ = deserialize(jsonDataBasic);
+const lookupValS = merkleMapBasic_.get(Field(fourAces));
+console.log(lookupValS.toString());
+assert.equal(lookupValS.toString(), "10", 'Serialized+load lookup should be 10');
 
 await shutdown();
