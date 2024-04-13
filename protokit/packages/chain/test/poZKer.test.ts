bef9f579b4e26c40e808250f0271c24cc719fc24
Thomas <thomasr928@gmail.com>
2024-04-13 09:32:34 -0500

    Add test for deposit method

--- protokit/packages/chain/test/poZKer.test.ts
+++ protokit/packages/chain/test/poZKer.test.ts
@@ -1,6 +1,6 @@
 import { TestingAppChain } from "@proto-kit/sdk";
-import { PrivateKey, Field, Poseidon } from "o1js";
-import { Balances } from "../src/poker";
+import { PrivateKey, Field, Poseidon, PublicKey } from "o1js";
+import { Balances } from "../src/poZKer";
 import { log } from "@proto-kit/common";
 import { BalancesKey, TokenId, UInt64 } from "@proto-kit/library";
 
@@ -12,7 +12,6 @@ describe("poZKer", () => {
     const appChain = TestingAppChain.fromRuntime({
       Balances,
     });
-
     appChain.configurePartial({
       Runtime: {
         Balances: {
@@ -20,10 +19,23 @@ describe("poZKer", () => {
         },
       },
     });
-
     await appChain.start();
     return appChain;
   }
+
+  async function setPlayer(appChain: any, balances: Balances, playerPrivKey: PrivateKey, playerPubKey: PublicKey) {
+    // Join game with two players
+
+    // First player joining
+    appChain.setSigner(playerPrivKey);
+    const tx1 = await appChain.transaction(playerPubKey, () => {
+      balances.joinGame(playerPubKey)
+    });
+    await tx1.sign();
+    await tx1.send();
+    const block = await appChain.produceBlock();
+  }
+
 
   it("allows players to join game (joinGame)", async () => {
 
@@ -34,8 +46,8 @@ describe("poZKer", () => {
     const alice = alicePrivateKey.toPublicKey();
     const aliceHash = Poseidon.hash(alice.toFields());
     const bobPrivateKey = PrivateKey.random();
-    const bob = alicePrivateKey.toPublicKey();
-    const bobHash = Poseidon.hash(alice.toFields());
+    const bob = bobPrivateKey.toPublicKey();
+    const bobHash = Poseidon.hash(bob.toFields());
 
     appChain.setSigner(alicePrivateKey);
 
@@ -57,7 +69,7 @@ describe("poZKer", () => {
 
 
     // Second player joining
-    // appChain.setSigner(bobPrivateKey);
+    appChain.setSigner(bobPrivateKey);
     const tx2 = await appChain.transaction(bob, () => {
       balances.joinGame(bob)
     });
@@ -72,4 +84,47 @@ describe("poZKer", () => {
     expect(player2HashB).toEqual(bobHash);
 
   }, 1_000_000);
+
+
+  it("allows players to deposit (deposit())", async () => {
+    const appChain = await localDeploy();
+    const alicePrivateKey = PrivateKey.random();
+    const bobPrivateKey = PrivateKey.random();
+    const alice = alicePrivateKey.toPublicKey();
+    const bob = bobPrivateKey.toPublicKey();
+    appChain.setSigner(alicePrivateKey);
+    const balances = appChain.runtime.resolve("Balances");
+
+    await setPlayer(appChain, balances, alicePrivateKey, alice);
+    await setPlayer(appChain, balances, bobPrivateKey, bob);
+
+    const depositAmount1: Field = Field(100);
+    const depositAmount2: Field = Field(120);
+
+    appChain.setSigner(alicePrivateKey);
+    const tx1 = await appChain.transaction(alice, () => {
+      balances.deposit(depositAmount1)
+    });
+    await tx1.sign();
+    await tx1.send();
+    const block = await appChain.produceBlock();
+
+    appChain.setSigner(bobPrivateKey);
+    const tx2 = await appChain.transaction(bob, () => {
+      balances.deposit(depositAmount2)
+    });
+    await tx2.sign();
+    await tx2.send();
+    const block2 = await appChain.produceBlock();
+
+    const stack1 = await appChain.query.runtime.Balances.stack1.get();
+    const stack2 = await appChain.query.runtime.Balances.stack2.get();
+    expect(block?.transactions[0].status.toBoolean()).toBe(true);
+    expect(block2?.transactions[0].status.toBoolean()).toBe(true);
+    // For actual stacks we'll have to subtract the blinds (1/2)
+    expect(stack1).toEqual(depositAmount1.sub(Field(1)));
+    expect(stack2).toEqual(depositAmount2.sub(Field(2)));
+
+  }, 1_000_000);
+
 });
