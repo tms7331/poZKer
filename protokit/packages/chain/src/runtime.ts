161689f15a73496f37370a4662bc314f46fb0b06
Thomas <thomasr928@gmail.com>
2024-04-15 14:37:10 -0500

    Add poZKer to runtime

--- protokit/packages/chain/src/runtime.ts
+++ protokit/packages/chain/src/runtime.ts
@@ -1,12 +1,15 @@
 import { Balance } from "@proto-kit/library";
 import { Balances } from "./balances";
+import { PoZKerApp } from "./poZKer";
 import { ModulesConfig } from "@proto-kit/common";
 
 export const modules = {
+  PoZKerApp,
   Balances,
 };
 
 export const config: ModulesConfig<typeof modules> = {
+  PoZKerApp: {},
   Balances: {
     totalSupply: Balance.from(10_000),
   },
