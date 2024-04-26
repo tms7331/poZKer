f5aa14cab955ca78fac8fc2f037e423a0df8db27
m4nute <mgudinocontacto@gmail.com>
2024-04-26 13:48:44 -0300

    Avoid crashing on playgame-ui after merging thomas progress (breaks imports)

--- protokit/apps/web/containers/playgame-page-ui.tsx
+++ protokit/apps/web/containers/playgame-page-ui.tsx
@@ -5,8 +5,6 @@ import { Field, PublicKey, PrivateKey } from "o1js";
 import {
   usePoZKerStore,
   useShowCards,
-  useShowdown,
-  useWithdraw,
   useTakeAction,
 } from "@/lib/stores/poZKer";
 import { useWalletStore } from "@/lib/stores/wallet";
@@ -15,8 +13,6 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 export default function Component() {
   const showCards = useShowCards();
-  const showdown = useShowdown();
-  const withdraw = useWithdraw();
   const takeAction = useTakeAction();
   const wallet = useWalletStore();
   const pkrState = usePoZKerStore();
@@ -44,200 +40,200 @@ export default function Component() {
   type Player = "player1" | "player2" | "notInGame";
   const [player, setPlayer] = useState<Player>("notInGame");
 
-  useEffect(() => {
-    // TODO - where can we get this?
-    const userKey = wallet.wallet;
-    console.log("userKey is:", userKey);
+  // useEffect(() => {
+  //   // TODO - where can we get this?
+  //   const userKey = wallet.wallet;
+  //   console.log("userKey is:", userKey);
 
-    // Figure out which player we are...
-    if (userKey === pkrState.player1Key) {
-      console.log("Matched player 1...");
-      setPlayer("player1");
-      // Hardcoding player's cards
-      setHoleCards(["Ah", "Ad"]);
-    } else if (userKey === pkrState.player2Key) {
-      console.log("Matched player 2...");
-      setPlayer("player2");
-      setHoleCards(["Ks", "Ts"]);
-    } else {
-      console.log("Not in game...");
-      setPlayer("notInGame");
-    }
-  }, [pkrState.player1Key, pkrState.player2Key, wallet.wallet]);
+  //   // Figure out which player we are...
+  //   if (userKey === pkrState.player1Key) {
+  //     console.log("Matched player 1...");
+  //     setPlayer("player1");
+  //     // Hardcoding player's cards
+  //     setHoleCards(["Ah", "Ad"]);
+  //   } else if (userKey === pkrState.player2Key) {
+  //     console.log("Matched player 2...");
+  //     setPlayer("player2");
+  //     setHoleCards(["Ks", "Ts"]);
+  //   } else {
+  //     console.log("Not in game...");
+  //     setPlayer("notInGame");
+  //   }
+  // }, [pkrState.player1Key, pkrState.player2Key, wallet.wallet]);
 
-  const onSendTransaction = async (methodStr: string, actionStr: string) => {
-    // await globalState.zkappWorkerClient!.fetchAccount({
-    //     publicKey: globalState.publicKey!
-    // });
+  // const onSendTransaction = async (methodStr: string, actionStr: string) => {
+  //   // await globalState.zkappWorkerClient!.fetchAccount({
+  //   //     publicKey: globalState.publicKey!
+  //   // });
 
-    // Needed for several of the transactions
-    switch (methodStr) {
-      case "takeAction":
-        const actionMapping: { [key: string]: number } = {
-          // "Null": UInt32.from(0),
-          Bet: 1,
-          Call: 2,
-          Fold: 3,
-          Raise: 4,
-          Check: 5,
-          // We'll infer this one
-          // "PreflopCall": UInt32.from(6),
-        };
-        const action: number = actionMapping[actionStr];
-        const betSize: number = betAmount;
-        await takeAction(action, betSize);
-        // await globalState.zkappWorkerClient!.createTakeActionTx(senderB58, action, betSize);
-        break;
-      case "showdown":
-        await showdown();
-        break;
-      case "withdraw":
-        await withdraw();
-        break;
-      case "tallyBoardCards":
-        const cardPrime52: number = 0;
-        // useTallyBoardCardsTx(cardPrime52);
-        break;
-      case "showCards":
-        // Hardcoded values for now...
+  //   // Needed for several of the transactions
+  //   switch (methodStr) {
+  //     case "takeAction":
+  //       const actionMapping: { [key: string]: number } = {
+  //         // "Null": UInt32.from(0),
+  //         Bet: 1,
+  //         Call: 2,
+  //         Fold: 3,
+  //         Raise: 4,
+  //         Check: 5,
+  //         // We'll infer this one
+  //         // "PreflopCall": UInt32.from(6),
+  //       };
+  //       const action: number = actionMapping[actionStr];
+  //       const betSize: number = betAmount;
+  //       await takeAction(action, betSize);
+  //       // await globalState.zkappWorkerClient!.createTakeActionTx(senderB58, action, betSize);
+  //       break;
+  //     case "showdown":
+  //       await showdown();
+  //       break;
+  //     case "withdraw":
+  //       await withdraw();
+  //       break;
+  //     case "tallyBoardCards":
+  //       const cardPrime52: number = 0;
+  //       // useTallyBoardCardsTx(cardPrime52);
+  //       break;
+  //     case "showCards":
+  //       // Hardcoded values for now...
 
-        // "Ah": 41,
-        // "Ad": 101,
-        // "Ks": 233,
-        // "Ts": 223,
+  //       // "Ah": 41,
+  //       // "Ad": 101,
+  //       // "Ks": 233,
+  //       // "Ts": 223,
 
-        // "Kc": 163,
-        // "Ac": 167,
-        // "Qs": 229,
-        // "8s": 199,
-        // "6s": 193,
+  //       // "Kc": 163,
+  //       // "Ac": 167,
+  //       // "Qs": 229,
+  //       // "8s": 199,
+  //       // "6s": 193,
 
-        let holecard0: number;
-        let holecard1: number;
+  //       let holecard0: number;
+  //       let holecard1: number;
 
-        let useHolecard0: boolean;
-        let useHolecard1: boolean;
-        let useBoardcards0: boolean;
-        let useBoardcards1: boolean;
-        let useBoardcards2: boolean;
-        let useBoardcards3: boolean;
-        let useBoardcards4: boolean;
-        let isFlush: boolean;
-        let merkleMapKey: number;
-        let merkleMapVal: number;
+  //       let useHolecard0: boolean;
+  //       let useHolecard1: boolean;
+  //       let useBoardcards0: boolean;
+  //       let useBoardcards1: boolean;
+  //       let useBoardcards2: boolean;
+  //       let useBoardcards3: boolean;
+  //       let useBoardcards4: boolean;
+  //       let isFlush: boolean;
+  //       let merkleMapKey: number;
+  //       let merkleMapVal: number;
 
-        if (player === "player1") {
-          holecard0 = 41;
-          holecard1 = 101;
+  //       if (player === "player1") {
+  //         holecard0 = 41;
+  //         holecard1 = 101;
 
-          useHolecard0 = true;
-          useHolecard1 = true;
-          useBoardcards0 = true;
-          useBoardcards1 = true;
-          useBoardcards2 = true;
-          useBoardcards3 = false;
-          useBoardcards4 = false;
-          isFlush = false;
-          merkleMapKey = 79052387;
-          merkleMapVal = 1609;
-        } else if (player === "player2") {
-          holecard0 = 233;
-          holecard1 = 223;
+  //         useHolecard0 = true;
+  //         useHolecard1 = true;
+  //         useBoardcards0 = true;
+  //         useBoardcards1 = true;
+  //         useBoardcards2 = true;
+  //         useBoardcards3 = false;
+  //         useBoardcards4 = false;
+  //         isFlush = false;
+  //         merkleMapKey = 79052387;
+  //         merkleMapVal = 1609;
+  //       } else if (player === "player2") {
+  //         holecard0 = 233;
+  //         holecard1 = 223;
 
-          useHolecard0 = true;
-          useHolecard1 = true;
-          useBoardcards0 = false;
-          useBoardcards1 = false;
-          useBoardcards2 = true;
-          useBoardcards3 = true;
-          useBoardcards4 = true;
-          isFlush = true;
-          merkleMapKey = 4933247;
-          merkleMapVal = 858;
-        } else {
-          throw new Error("Not in game!");
-        }
+  //         useHolecard0 = true;
+  //         useHolecard1 = true;
+  //         useBoardcards0 = false;
+  //         useBoardcards1 = false;
+  //         useBoardcards2 = true;
+  //         useBoardcards3 = true;
+  //         useBoardcards4 = true;
+  //         isFlush = true;
+  //         merkleMapKey = 4933247;
+  //         merkleMapVal = 858;
+  //       } else {
+  //         throw new Error("Not in game!");
+  //       }
 
-        const boardcard0: number = 163;
-        const boardcard1: number = 167;
-        const boardcard2: number = 229;
-        const boardcard3: number = 199;
-        const boardcard4: number = 193;
+  //       const boardcard0: number = 163;
+  //       const boardcard1: number = 167;
+  //       const boardcard2: number = 229;
+  //       const boardcard3: number = 199;
+  //       const boardcard4: number = 193;
 
-        const shuffleKey: string = PrivateKey.fromBigInt(BigInt(1)).toBase58();
-        const path: string = "?";
+  //       const shuffleKey: string = PrivateKey.fromBigInt(BigInt(1)).toBase58();
+  //       const path: string = "?";
 
-        await showCards(
-          holecard0,
-          holecard1,
-          boardcard0,
-          boardcard1,
-          boardcard2,
-          boardcard3,
-          boardcard4,
-          useHolecard0,
-          useHolecard1,
-          useBoardcards0,
-          useBoardcards1,
-          useBoardcards2,
-          useBoardcards3,
-          useBoardcards4,
-          isFlush,
-          shuffleKey,
-          merkleMapKey,
-          merkleMapVal,
-          path,
-        );
-        break;
-    }
-  };
+  //       await showCards(
+  //         holecard0,
+  //         holecard1,
+  //         boardcard0,
+  //         boardcard1,
+  //         boardcard2,
+  //         boardcard3,
+  //         boardcard4,
+  //         useHolecard0,
+  //         useHolecard1,
+  //         useBoardcards0,
+  //         useBoardcards1,
+  //         useBoardcards2,
+  //         useBoardcards3,
+  //         useBoardcards4,
+  //         isFlush,
+  //         shuffleKey,
+  //         merkleMapKey,
+  //         merkleMapVal,
+  //         path,
+  //       );
+  //       break;
+  //   }
+  // };
 
   // TODO - we'll need multiple functions like this to convert our encoding to strings
-  useEffect(() => {
-    // Set board based on current street...
-    // type Street = "ShowdownPending" | "Preflop" | "Flop" | "Turn" | "River" | "ShowdownComplete";
-    const boardStr: string = pkrState.street;
-    const streetMap: { [key: string]: string } = {
-      "1": "ShowdownPending",
-      "2": "Preflop",
-      "3": "Flop",
-      "4": "Turn",
-      "5": "River",
-      "6": "ShowdownComplete",
-    };
-    const streetStr = streetMap[boardStr];
-    // setStreet(streetStr);
+  // useEffect(() => {
+  //   // Set board based on current street...
+  //   // type Street = "ShowdownPending" | "Preflop" | "Flop" | "Turn" | "River" | "ShowdownComplete";
+  //   const boardStr: string = pkrState.street;
+  //   const streetMap: { [key: string]: string } = {
+  //     "1": "ShowdownPending",
+  //     "2": "Preflop",
+  //     "3": "Flop",
+  //     "4": "Turn",
+  //     "5": "River",
+  //     "6": "ShowdownComplete",
+  //   };
+  //   const streetStr = streetMap[boardStr];
+  //   // setStreet(streetStr);
 
-    // const lastActionStr: string = lastAction_.toJSON()
-    const actionMap: { [key: string]: string } = {
-      "0": "Null",
-      "1": "Bet",
-      "2": "Call",
-      "3": "Fold",
-      "4": "Raise",
-      "5": "Check",
-      "6": "PreflopCall",
-    };
-    // const actionStr = actionMap[lastActionStr];
-    // setLastAction(actionStr);;
+  //   // const lastActionStr: string = lastAction_.toJSON()
+  //   const actionMap: { [key: string]: string } = {
+  //     "0": "Null",
+  //     "1": "Bet",
+  //     "2": "Call",
+  //     "3": "Fold",
+  //     "4": "Raise",
+  //     "5": "Check",
+  //     "6": "PreflopCall",
+  //   };
+  //   // const actionStr = actionMap[lastActionStr];
+  //   // setLastAction(actionStr);;
 
-    // Street encoding
-    // Preflop = UInt32.from(2);
-    // Flop = UInt32.from(3);
-    // Turn = UInt32.from(4);
-    // River = UInt32.from(5);
-    if (boardStr === "3") {
-      const board = boardCardsHardcoded.slice(0, 3);
-      setBoardCards(board);
-    } else if (boardStr === "4") {
-      const board = boardCardsHardcoded.slice(0, 4);
-      setBoardCards(board);
-    } else if (boardStr === "5") {
-      setBoardCards(boardCardsHardcoded);
-    } else {
-      setBoardCards([]);
-    }
-  }, []);
+  //   // Street encoding
+  //   // Preflop = UInt32.from(2);
+  //   // Flop = UInt32.from(3);
+  //   // Turn = UInt32.from(4);
+  //   // River = UInt32.from(5);
+  //   if (boardStr === "3") {
+  //     const board = boardCardsHardcoded.slice(0, 3);
+  //     setBoardCards(board);
+  //   } else if (boardStr === "4") {
+  //     const board = boardCardsHardcoded.slice(0, 4);
+  //     setBoardCards(board);
+  //   } else if (boardStr === "5") {
+  //     setBoardCards(boardCardsHardcoded);
+  //   } else {
+  //     setBoardCards([]);
+  //   }
+  // }, []);
 
   // stack, facing bet, action history,  board_cards, hole_cards, pot
   // This is our bet amount
@@ -303,7 +299,8 @@ export default function Component() {
               <div className="absolute inset-x-0 -bottom-1 flex h-12 flex-col justify-center rounded-xl border-2 border-black bg-[#313390] px-4 py-2 text-white">
                 <div className="flex">
                   <span className="font-bold">Opponent</span>
-                  <span className="flex-1 text-center">${pkrState.stack2}</span>
+                  <span className="flex-1 text-center">${"0"}</span>
+                  {/* pkrState.stack2 || */}
                 </div>
               </div>
               <Image
