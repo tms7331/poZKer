51c6e6f0941a7a60d627471c0a7ba9e28c2c7449
m4nute <mgudinocontacto@gmail.com>
2024-04-30 16:38:08 -0300

    Set overall background to be #111

--- protokit/apps/web/app/layout.tsx
+++ protokit/apps/web/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={`h-full bg-background font-sans antialiased ${fontSans.className}`}
+        className={`h-full bg-[#111] font-sans antialiased ${fontSans.className}`}
       >
         <AsyncLayoutDynamic>{children}</AsyncLayoutDynamic>
       </body>
