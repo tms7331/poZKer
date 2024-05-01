db87e1b1beb177a038405f1fbc43d4d848202a01
Thomas <thomasr928@gmail.com>
2024-05-01 11:38:29 -0500

    Remove old UI page

--- protokit/apps/web/app/playgame-ui/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import "reflect-metadata";
-import PlaygamePageDynamic from "@/containers/playgame-page-dynamic-ui";
-
-export default function Component() {
-  return <PlaygamePageDynamic />;
-}
